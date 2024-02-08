fba0bee24e312b0de9e1f0e2b3b9fb8ba3c3d540
Lakkappa Basidoni <95579704+LAKKAPPA-BASIDONI@users.noreply.github.com>
2024-02-08 18:09:42 +0530

    Update CricbuzzKwd

--- Keywords/CricbuzzKwd.groovy
+++ Keywords/CricbuzzKwd.groovy
@@ -50,7 +50,7 @@ public class CricbuzzKwd {
 		WebUI.click(locator.firstMatch())
 		WebUI.click(locator.scoreCardLink())
 		Cricketformat = WebUI.getText(locator.formatName())
-		println("******** "+Cricketformat+" *********")
+		println("***ABC***** "+Cricketformat+" ***ABC******")
 	}
 
 	def playerInformationCollect(String teamName) {
